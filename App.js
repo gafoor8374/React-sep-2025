b59b3e65609a907e25c4e108e4ee552801b4c0e4
Shaik Gafoor <shaikgafoor@Shaiks-MacBook-Air.local>
2025-09-21 00:42:37 +0530

    Episode02

--- App.js
+++ App.js
@@ -1,38 +1,18 @@
-/**
- * 
- * <div id = "parent">
- *    <div id = "child">
- *     <h1>I am h1 tag</h1>
- *     <h2>I am h1 tag</h2>
- *   </div>
- * <div id = "child2">
- *     <h1>I am h1 tag</h1>
- *     <h2>I am h1 tag</h2>
- *   </div>
- * </div>
- * 
- * 
- * 
- * 
- * 
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am an h1 tag"),
-    React.createElement("h2", {}, "I am an h2 tag"),
+    React.createElement("div", { id: "child", key:"childkey" }, [
+    React.createElement("h1",{id:"h1tag", key:"h1key"}, "I am an h1 tag"),
+    React.createElement("h2", {id:"h2tag", key:"h2key"}, "I am an h2 tag"),
   ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am an h1 tag"),
-    React.createElement("h2", {}, "I am an h2 tag"),
-  ]),
-]);
+  React.createElement("div", { id: "child2", key:"child2key" }, [
+    React.createElement("h1",{id:"h1tag", key:"h1key"}, "I am an h1 tag"),
+    React.createElement("h2", {id:"h2tag", key:"h2key"}, "I am an h2 tag"),
+  ])
+]
 
-// const heading = React.createElement(
-//     "h1", 
-//     {id:"heading"}, 
-//     "Hello world from React🥰"
-// );
+);
 
 console.log(parent);
 
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Learning React 🥰
+
+
+# Parcel
+- Dev Build
+- local server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written c++
+- Caching - Faster builds
+- image optimazation
+- Minification of the files
+- Bundling
+- Comparess
+- Consistent Hashing
+- Code splitting
+- Differential Bundling - support older browsers
+- Error handling
+- Diagnostics
+- Host your app in HTTPs
+- Tree Shaking - remove unused code from project
+- Build dev and production 
